37731a99743df25161cb90ae703db76cce20087a
yearswriter <38693248+yearswriter@users.noreply.github.com>
2025-11-11 13:48:34 +0300

    tusing 'tracing' for logs, lest stdout

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,26 +1,67 @@
 use tokio::io::{self, AsyncWriteExt};
 use tokio::net::TcpSocket;
 use tokio::task::JoinSet;
+use tracing::{error, info};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let addr = "10.204.1.2:8150".parse().unwrap();
+    let file_appender = tracing_appender::rolling::hourly("logs", "client");
+    let (writer, _guard) = tracing_appender::non_blocking(file_appender);
+
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .with_writer(writer)
+        .init();
+
+    let addr = "127.0.0.1:8150".parse().unwrap();
     let mut tasks = JoinSet::new();
 
     for task in 1..1000 {
         tasks.spawn(async move {
-            let socket = TcpSocket::new_v4()
-                .expect(&format!("failed to create the socket: {task}"));
-            let task_bytes = (task as u16).to_be_bytes(); // Big Endian [u8;2]
-            let buf: [u8; 8] = [task_bytes[0], task_bytes[1], 0xD, 0xC, 0xB, 0xA, 9, 8];
-            let mut stream = socket
-                .connect(addr)
-                .await
-                .expect(&format!("failed to connect the socket: {task}"));
-            stream
-                .write_all(&buf.clone())
-                .await
-                .expect(&format!("failed to write the stream: {task}"));
+            let socket = match TcpSocket::new_v4() {
+                Ok(socket) => socket,
+                Err(error) => {
+                    error!(
+                        "Failed to create the socket, task: {}, error: {:?}",
+                        task, error
+                    );
+                    panic!("Failed to create the socket");
+                }
+            };
+            let task_bytes = (task as u32).to_be_bytes(); // Big Endian [u8;2]
+            let buf: [u8; 8] = [
+                task_bytes[0],
+                task_bytes[1],
+                task_bytes[2],
+                task_bytes[3],
+                0xB,
+                0xA,
+                9,
+                8,
+            ];
+            let mut stream = match socket.connect(addr).await {
+                Ok(socket) => socket,
+                Err(error) => {
+                    error!(
+                        "Failed to connect to the socket, task: {}, error: {:?}",
+                        task, error
+                    );
+                    panic!("Failed to connect to the socket");
+                }
+            };
+            match stream.write_all(&buf.clone()).await {
+                Ok(socket) => {
+                    info!("writing to the stream from task: {}", task);
+                    socket
+                }
+                Err(error) => {
+                    error!(
+                        "Failed to write to the stream, task: {}, error: {:?}",
+                        task, error
+                    );
+                    panic!("Failed to write to the stream");
+                }
+            };
         });
     }
 
@@ -29,7 +70,7 @@ async fn main() -> io::Result<()> {
         // We loop until it returns None (meaning the set is empty).
         // `res` is a Result<T, JoinError>, so we check for panics.
         if let Err(e) = res {
-            eprintln!("A task panicked: {:?}", e);
+            error!("A task panicked: {:?}", e);
             break;
         }
     }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,29 +1,68 @@
+use std::net::SocketAddr;
+use std::os::unix::io::AsRawFd;
+use std::sync::Arc;
 use tokio::io::{self, AsyncReadExt};
-use tokio::net::{TcpListener, TcpStream};
+use tokio::net::{TcpListener, TcpSocket, TcpStream};
 use tokio::signal;
+use tokio::time::{Duration, timeout};
 use tokio_util::sync::CancellationToken;
+use tracing::{error, info, instrument, warn};
 
+#[instrument(skip_all)]
 async fn process_socket(mut socket: TcpStream, token: CancellationToken) {
-    dbg!(&socket);
     let mut buf = [0u8; 8];
+    // timeout for our clients so no zombie holds the connection
+    let read_timeout = Duration::from_secs(5);
 
-    match socket.read_exact(&mut buf).await {
-        Ok(_) => {
-            let received_task: [u8; 2] = buf[0..2].try_into().unwrap();
-            let task_id = u16::from_be_bytes(received_task);
-            println!("Received task: {}", task_id);
+    match timeout(read_timeout, socket.read_exact(&mut buf)).await {
+        Ok(Ok(_)) => {
+            let received_task: [u8; 4] = match buf[0..4].try_into() {
+                Ok(task) => task,
+                Err(error) => {
+                    error!("Failed to slice buff for task_id: {:?}", error);
+                    token.cancel();
+                    return;
+                }
+            };
+            let task_id = u32::from_be_bytes(received_task);
+            info!(
+                "Received task: {}, from fd: {:#}",
+                task_id,
+                socket.as_raw_fd()
+            );
         }
-        Err(e) => {
-            eprintln!("Failed to read: {}", e);
-            token.cancel();
+        Ok(Err(read_error)) => {
+            error!("Failed to read: {}", read_error);
         }
-    }
+        Err(_) => {
+            warn!("Client timed out from fd: {}", socket.as_raw_fd());
+        }
+    };
+}
+
+async fn create_listener(addr: SocketAddr) -> io::Result<TcpListener> {
+    // need this over TcListener::bind(addr) to enlarge OS backlog for tcp bind
+    let socket = TcpSocket::new_v4()?;
+    socket.set_reuseaddr(true)?;
+    socket.bind(addr)?;
+    let listener = socket.listen(4096)?;
+    Ok(listener)
 }
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let listener = TcpListener::bind("0.0.0.0:8150").await?;
+    let addr: SocketAddr = "0.0.0.0:8150".parse().unwrap();
+    let listener = create_listener(addr).await?;
+
     let token = CancellationToken::new();
+
+    let file_appender = tracing_appender::rolling::hourly("logs", "server");
+    let (writer, _guard) = tracing_appender::non_blocking(file_appender);
+
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .with_writer(writer)
+        .init();
 
     loop {
         tokio::select! {
@@ -38,6 +77,14 @@ async fn main() -> io::Result<()> {
             println!("\nReceived Ctrl+C, shutting down!");
             break;
         }
+            _ = token.cancelled() => {
+                info!("\nTask canceled token, shutting down!");
+                break;
+            }
+            Ok(()) = signal::ctrl_c() => {
+                println!("\nReceived Ctrl+C, shutting down!");
+                break;
+            }
         }
     }
     Ok(())
